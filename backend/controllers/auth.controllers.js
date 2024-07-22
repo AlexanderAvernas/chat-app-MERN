1c4b647880be727484a47f5a8178d7fa4b503f62
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2024-07-22 07:29:36 +0200

    Added hooks and frontend

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -4,10 +4,10 @@ import generateTokenAndSetCookie from '../utils/generateToken.js'
 
 export const signup = async (req, res) => {
     try {
-        const { fullName, username, password, confirmpassword, gender } =
+        const { fullName, username, password, confirmPassword, gender } =
             req.body
 
-        if (password !== confirmpassword) {
+        if (password !== confirmPassword) {
             return res.status(400).json({ error: 'Password dont match' })
         }
 
